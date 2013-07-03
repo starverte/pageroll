e70089489cc328ab8c1b5a45598e2737ae557a2d
Matt Beall <mbeall@starverte.com>
2013-07-03 12:54:57 -0500

    Create front-page.php

---
+++ front-page.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the front page
+ *
+ * Template file used to render the Site Front Page,
+ * whether the front page displays the Blog Posts Index
+ * or a static page.
+ *
+ * @package Flint
+ *
+ */
+
+get_header(); ?>
+
+  	<div id="primary" class="content-area span9">
+			<div id="content" class="site-content" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', get_post_format() ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+        
+        <?php flint_content_nav( 'nav-below' ); ?>
+
+			</div><!-- #content .site-content -->
+		</div><!-- #primary .content-area -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
