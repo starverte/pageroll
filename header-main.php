82b37a052167d6c4c772773a72ac59a807539c60
Matt Beall <mbeall@starverte.com>
2013-08-03 00:14:29 -0600

    Update navbar for Bootstrap 3 RC 1 and drop hgroup element

--- header-main.php
+++ header-main.php
@@ -2,7 +2,7 @@
 /**
  * Part of the Header for our theme.
  *
- * Displays all of the <head> section and everything from <div id="page"> through <hgroup>
+ * Displays all of the <head> section and everything from <div id="page"> through <div>
  *
  * @package Flint
  * @sub-package Pageroll
@@ -12,40 +12,35 @@
 	<?php do_action( 'before' ); ?>
 	<header id="masthead" class="site-header" role="banner">
     	<nav role="navigation" class="navbar navbar-inverse navbar-fixed-top">
-            <h1 class="screen-reader-text"><?php _e( 'Menu', 'flint' ); ?></h1>
-            <div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'flint' ); ?>"><?php _e( 'Skip to content', 'flint' ); ?></a></div>
-            <div class="navbar-inner">
-                <div class="container">
-                    
-                    <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    </a>
-                    
-                    <!-- Be sure to leave the brand out there if you want it shown -->
-                    <a class="brand hidden-desktop" href="<?php echo home_url(); ?>"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a>
-                    
-                    <div class="nav-collapse collapse">
-                    	<?php wp_nav_menu( array( 'menu_class' => 'nav', 'container' => false, 'theme_location' => 'primary', 'walker' => new Pageroll_Bootstrap_Menu ) ); ?>
-                        <?php if ( is_user_logged_in() ) { ?>
-                        <form class="navbar-form pull-right">                          
-													<a class="btn btn-large btn-block hidden-desktop" href="<?php echo wp_logout_url( get_permalink() ); ?>"><i class="icon-remove"></i> Logout</a>
-                        	<a class="btn visible-desktop" href="<?php echo wp_logout_url( get_permalink() ); ?>"><i class="icon-remove"></i> Logout</a>
-                        </form>
-                        <?php } ?>
-                    </div><!-- .nav-collapse -->
-                </div><!-- .container -->
-            </div><!-- .navbar-inner -->
-        </nav><!-- .navbar -->
+			<h1 class="screen-reader-text"><?php _e( 'Menu', 'flint' ); ?></h1>
+			<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'flint' ); ?>"><?php _e( 'Skip to content', 'flint' ); ?></a></div>
+      <div class="container">
+              
+        <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+                  
+        <!-- Be sure to leave the brand out there if you want it shown -->
+        <a class="navbar-brand hidden-lg" href="<?php echo home_url(); ?>"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a>
+                  
+        <div class="nav-collapse collapse navbar-responsive-collapse">
+          <?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => false, 'menu_class' => 'nav navbar-nav', 'fallback_cb' => false, 'walker' => new Pageroll_Bootstrap_Menu ) ); ?>
+          <form method="get" class="navbar-form pull-right" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+            <input type="text" class="form-control" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" placeholder="Search" style="width: 200px;">
+          </form>
+        </div><!-- .nav-collapse -->
+      </div><!-- .container -->
+		</nav><!-- .navbar -->
 	<?php if (current_theme_supports('custom-header')) { ?>
-		<hgroup class="row-fluid">
+		<div class="container">
 		<?php $header_image = get_header_image();
 		if ( ! empty( $header_image ) ) { ?>
-			<a class="logo span3 hidden-phone" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+			<a class="logo col-lg-3 col-offset-1 hidden-sm" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 				<img src="<?php header_image(); ?>" alt="" />
 			</a>
 		<?php } /* if ( ! empty( $header_image ) ) */ ?>
-		</hgroup>
+		</div>
 	<?php } /* if (current_theme_supports('custom-header')) */ ?>
